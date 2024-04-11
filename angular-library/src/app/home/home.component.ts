574758f6af002a18cb7f21472d1cdcbfb1db7884
Iordan Bogdanov <iordanbogdanov88@gmail.com>
2024-04-11 23:44:10 +0300

    added home component

---
+++ angular-library/src/app/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { WelcomeMsgComponent } from '../shared/welcome-msg/welcome-msg.component';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent {
+
+}
--- angular-library/src/app/shared/shared.module.ts
+++ angular-library/src/app/shared/shared.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { WelcomeMsgComponent } from './welcome-msg/welcome-msg.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [WelcomeMsgComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [
+    WelcomeMsgComponent,
+  ],
 })
 export class SharedModule { }
