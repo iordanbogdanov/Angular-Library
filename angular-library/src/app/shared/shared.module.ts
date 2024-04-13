675dd19f1bd9e4f8de981276a8702962550261ae
Iordan Bogdanov <iordanbogdanov88@gmail.com>
2024-04-13 21:58:44 +0300

    slicePipe imported to sharedModule

--- angular-library/src/app/shared/shared.module.ts
+++ angular-library/src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WelcomeMsgComponent } from './welcome-msg/welcome-msg.component';
 import { RouterModule } from '@angular/router';
+import { SlicePipe } from './pipes/slice.pipe';
 
 @NgModule({
-  declarations: [WelcomeMsgComponent],
+  declarations: [WelcomeMsgComponent, SlicePipe],
   imports: [CommonModule, RouterModule],
-  exports: [WelcomeMsgComponent],
+  exports: [WelcomeMsgComponent, SlicePipe],
 })
 export class SharedModule {}
